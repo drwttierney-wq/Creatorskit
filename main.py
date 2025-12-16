a30dbca53d7a95615517172bbf2af19e3ce0e293
drwttierney-wq <drwttierney@gmail.com>
2025-12-16 22:33:30 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
     return render_template("Index.html")
+
+@app.route("/tiktok")
+def tiktok():
+    return render_template("Tictok.html")
+
+@app.route("/instagram")
+def instagram():
+    return render_template("instagram.html")
+
+@app.route("/youtube")
+def youtube():
+    return render_template("youtube.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
