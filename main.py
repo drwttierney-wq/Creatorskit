b60c57bfb552166ea7592da9d52f55ecb045623b
drwttierney-wq <drwttierney@gmail.com>
2025-12-17 14:42:12 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, abort
+from flask import request, redirect, url_for, session
+from database import init_db, get_dbfrom flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
@@ -55,5 +56,4 @@ def platform_page(platform):
     )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)from flask import request, redirect, url_for, session
-from database import init_db, get_db
+    app.run(host="0.0.0.0", port=8080)
