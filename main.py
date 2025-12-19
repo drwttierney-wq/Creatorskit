6fbd98271e58959697b5c63308ac9b1c48db26e2
drwttierney-wq <drwttierney@gmail.com>
2025-12-19 21:55:12 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
