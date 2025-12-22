cb3b6d49cdb74a38b0aa58a55fb7b375aee43fa2
drwttierney-wq <drwttierney@gmail.com>
2025-12-22 17:44:38 +0000

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,3 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
