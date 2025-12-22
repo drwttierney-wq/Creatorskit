ec5dd227d98c1b29bec1e9685d69017ee4f82132
drwttierney-wq <drwttierney@gmail.com>
2025-12-22 14:40:30 +0000

    Update database.py

--- database.py
+++ database.py
@@ -1,46 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-from main import app
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
-# ----------------------
-# MODELS
-# ----------------------
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
-    posts = db.relationship('Post', backref='user', lazy=True)
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.Text, nullable=False)
-    image = db.Column(db.String(300))
-    attached_idea = db.Column(db.Text)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
-    likes = db.relationship('Like', backref='post', lazy=True)
-
-class Like(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-class Follow(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    follower_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    followed_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    content = db.Column(db.Text)
-    image = db.Column(db.String(300))
-    timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
-
-# ----------------------
-# INITIALIZE DB
-# ----------------------
-with app.app_context():
-    db.create_all()
